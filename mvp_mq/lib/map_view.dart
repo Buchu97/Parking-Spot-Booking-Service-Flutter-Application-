6fb233cdbc687ee1b667f8cdd2bdca759123870b
unknown <budhilc@gmail.com>
2024-05-03 19:01:38 +1000

    using InteractiveViewer widget instead of PhotoView for betterdisplay of the campus map

--- mvp_mq/lib/map_view.dart
+++ mvp_mq/lib/map_view.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
-// import 'package:photo_view/photo_view_gallery.dart';
+
 
 class MapWidget extends StatelessWidget {
   const MapWidget({super.key,required this.imagePath});
   final String imagePath;
+  
+  
 
   
 
   @override
   Widget build(BuildContext context) {
-    return PhotoView(
-      imageProvider: AssetImage(imagePath),
-      minScale: PhotoViewComputedScale.contained * 0.8,
-      maxScale: PhotoViewComputedScale.covered * 2,
-      backgroundDecoration: const BoxDecoration(
-        color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        // title: const Text('B'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: Colors.pink,
       ),
-    );
+      
+       
+       
+        body: Container(
+          color: Colors.pink[100],
+          child: Center( 
+           
+          child: InteractiveViewer( 
+            
+            
+            // boundaryMargin: const EdgeInsets.all(50.0), // Margin around the content 
+            minScale: 0.5, // Minimum scale (zoom out) 
+            maxScale: 2.0, // Maximum scale (zoom in) 
+            child: Image.network( 
+             imagePath, 
+              width: 1000.0, // Width of the initial image 
+              height: 700.0, // Height of the initial image 
+              fit: BoxFit.cover, // BoxFit to fill the available space 
+            ), 
+          ), 
+                ),
+        ), 
+          
+        );
+      
+    
   }
 }
+
