ef9c86079cfdc886c698858a6c7e663f3f75b8dc
unknown <budhilc@gmail.com>
2024-05-03 23:28:16 +1000

    using AspectRation widget for better map displaying adding error handiling

--- mvp_mq/lib/map_view.dart
+++ mvp_mq/lib/map_view.dart
@@ -1,51 +1,48 @@
 import 'package:flutter/material.dart';
 
-
 class MapWidget extends StatelessWidget {
-  const MapWidget({super.key,required this.imagePath});
+  const MapWidget({super.key, required this.imagePath});
   final String imagePath;
-  
-  
-
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // title: const Text('B'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.of(context).pop(),
         ),
         backgroundColor: Colors.pink,
       ),
-      
-       
-       
-        body: Container(
-          color: Colors.pink[100],
-          child: Center( 
-           
-          child: InteractiveViewer( 
-            
-            
-            // boundaryMargin: const EdgeInsets.all(50.0), // Margin around the content 
-            minScale: 0.5, // Minimum scale (zoom out) 
-            maxScale: 2.0, // Maximum scale (zoom in) 
-            child: Image.network( 
-             imagePath, 
-              width: 1000.0, // Width of the initial image 
-              height: 700.0, // Height of the initial image 
-              fit: BoxFit.cover, // BoxFit to fill the available space 
-            ), 
-          ), 
-                ),
-        ), 
+      body: Container(
+        
+        color: Colors.pink[100],
+        child: Center(
           
-        );
-      
-    
+          child: InteractiveViewer(
+            
+            boundaryMargin: const EdgeInsets.all(20.0),
+            minScale: 0.5,
+            maxScale: 2.0,
+            child: AspectRatio(
+              aspectRatio: 11 / 11,
+              child: Image.asset(
+                
+                imagePath,
+               
+                fit: BoxFit.fill,
+                errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                  print(exception.toString());
+                      if (stackTrace != null) {
+              print(stackTrace);
+                      }
+                      return const Text('Failed to load image');
+                  },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
-
