21283810611af35077e197e04cfea2114e93ad27
unknown <budhilc@gmail.com>
2024-05-03 13:53:04 +1000

    deleting the new parking pass record when the user press the cancel button on the parking pass page

--- mvp_mq/lib/parking_pass.dart
+++ mvp_mq/lib/parking_pass.dart
@@ -6,23 +6,24 @@ import 'package:mvp_mq/extension.dart';
 import 'package:mvp_mq/mq_app.dart';
 import 'package:mvp_mq/service_button.dart';
 
-class ParkingPass extends StatefulWidget{
+class ParkingPass extends StatefulWidget {
   const ParkingPass({super.key, required this.id});
   final int id;
 
- @override
- State<ParkingPass> createState(){
-  return _ParkingPass();
- }
+  @override
+  State<ParkingPass> createState() {
+    return _ParkingPass();
+  }
 }
 
-class _ParkingPass extends State<ParkingPass>{
-   Map<String, dynamic>? parkingPassData;
-   @override
+class _ParkingPass extends State<ParkingPass> {
+  Map<String, dynamic>? parkingPassData;
+  @override
   void initState() {
     super.initState();
     loadData();
   }
+
   void loadData() async {
     var data = await DatabaseHelper().getParkingPassById(widget.id);
     if (data != null) {
@@ -31,6 +32,7 @@ class _ParkingPass extends State<ParkingPass>{
       });
     }
   }
+
   Future<void> navigateAndRefresh() async {
     await Navigator.push(
       context,
@@ -38,66 +40,83 @@ class _ParkingPass extends State<ParkingPass>{
     );
     loadData();
   }
-@override
-  Widget build(context){
-  return  Scaffold(
-       appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          backgroundColor: Colors.pink,
-          elevation: 0,
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-         body: SingleChildScrollView(
-           child: Container(
-                   color: Colors.pink[100], // Set background color
-                   padding: const EdgeInsets.all(20),
-                   child: Column(
+        backgroundColor: Colors.pink,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.pink[100], // Set background color
+          padding: const EdgeInsets.all(20),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              
-           
-              FutureBuilder<Map<String,dynamic>?>(
-                   future: DatabaseHelper().getParkingPassById(widget.id),
-                   builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasData) {
-                return buildParkingPassContent(context, snapshot.data!);
-              } else {
-                return const Center(child: Text('No Parking Pass Found'));
-              }
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-                   },
-                 ),
-              
-              
-              ServiceButton(
-                text: "Extend Time",nextPage: navigateAndRefresh
-                // (){
-                //   Navigator.of(context).push(MaterialPageRoute(
-                //     builder: (context) =>Extension(id:id),
-                //   ));},
-                // style: ElevatedButton.styleFrom(
-                //   foregroundColor: Colors.pink, backgroundColor: Colors.white, // Text color
-                // ),
-                // onPressed: () {},
-                // child:const Text('Extend Time'),
+              FutureBuilder<Map<String, dynamic>?>(
+                future: DatabaseHelper().getParkingPassById(widget.id),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      return buildParkingPassContent(context, snapshot.data!);
+                    } else {
+                      return const Center(child: Text('No Parking Pass Found'));
+                    }
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                },
               ),
+              ServiceButton(text: "Extend Time", nextPage: navigateAndRefresh
+                  // (){
+                  //   Navigator.of(context).push(MaterialPageRoute(
+                  //     builder: (context) =>Extension(id:id),
+                  //   ));},
+                  // style: ElevatedButton.styleFrom(
+                  //   foregroundColor: Colors.pink, backgroundColor: Colors.white, // Text color
+                  // ),
+                  // onPressed: () {},
+                  // child:const Text('Extend Time'),
+                  ),
               const SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ServiceButton(
-                    text: "Cancel",nextPage: (){
-                      Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const MQApp(),
-                  ));
+                    text: "Cancel",
+                    nextPage: () async {
+                      
+                      int deletedCount = await DatabaseHelper().deleteParkingPass(widget.id);
+                      if(deletedCount != 0){
+                          ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Your parking pass has been deleted'),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                      Future.delayed(const Duration(seconds: 2), () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const MQApp(),
+                        ));
+                      });
+                      }else{
+                        ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Deleting Parking Pass was unsuccessful'),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                      }
+                    
                     },
-           
+
                     // style: ElevatedButton.styleFrom(
                     //   foregroundColor: Colors.pink, backgroundColor: Colors.white, // Text color
                     // ),
@@ -105,19 +124,20 @@ class _ParkingPass extends State<ParkingPass>{
                     // child: const Text('Cancel'),
                   ),
                   ServiceButton(
-                    text: "Pay",nextPage: (){},
+                    text: "Pay",
+                    nextPage: () {},
                   ),
-                  
                 ],
               ),
             ],
-                   ),
-                 ),
-         ),
+          ),
+        ),
+      ),
     );
   }
 
-   Widget buildParkingPassContent(BuildContext context, Map<String, dynamic> data) {
+  Widget buildParkingPassContent(
+      BuildContext context, Map<String, dynamic> data) {
     return Container(
       color: Colors.pink[100],
       padding: const EdgeInsets.all(20),
@@ -127,7 +147,8 @@ class _ParkingPass extends State<ParkingPass>{
         children: [
           const Text(
             'Your Parking Pass',
-            style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
             textAlign: TextAlign.center,
           ),
           const SizedBox(height: 20),
@@ -159,7 +180,8 @@ class _ParkingPass extends State<ParkingPass>{
       child: RichText(
         text: TextSpan(
           text: '$title ',
-          style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+          style: const TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
           children: <TextSpan>[
             TextSpan(
               text: value,
@@ -168,8 +190,6 @@ class _ParkingPass extends State<ParkingPass>{
           ],
         ),
       ),
-  );
-
-}
-
+    );
+  }
 }
