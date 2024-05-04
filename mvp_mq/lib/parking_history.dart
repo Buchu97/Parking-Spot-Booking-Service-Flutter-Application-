5957c671b30a8fea73b159fc8d7d34a026e28541
unknown <budhilc@gmail.com>
2024-05-04 19:49:08 +1000

    adding delete functionality for each parking passes

--- mvp_mq/lib/parking_history.dart
+++ mvp_mq/lib/parking_history.dart
@@ -18,7 +18,15 @@ class _ParkingHistoryScreenState extends State<ParkingHistoryScreen> {
     super.initState();
     parkingHistory = DatabaseHelper().getParkingPassHistory();
   }
-
+   void refreshParkingHistory() {
+    setState(() {
+      parkingHistory = DatabaseHelper().getParkingPassHistory();
+    });
+  }
+ Future<void> deleteParkingPass(int id) async {
+    await DatabaseHelper().deleteParkingPass(id);
+    refreshParkingHistory(); 
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,19 +47,32 @@ class _ParkingHistoryScreenState extends State<ParkingHistoryScreen> {
               itemCount: data.length,
               itemBuilder: (context, index) {
                 final parkingPass = data[index];
-                return Card(
-                  color: Colors.pink[50],
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.pink[300],
-                      child: Text('${parkingPass['id']}'),
-                    ),
-               
-                  title: Text(parkingPass['parkingLocation'] ?? 'Unknown Location'),
-                  subtitle: Text('Spot No: ${parkingPass['parkingSpotNo']} - Date: ${parkingPass['date']}'),
-                  trailing: Text('\$${parkingPass['amountPaid']}'),
-                ),
-                );
+                return Stack(
+                  children: [
+                  Card(
+                    color: Colors.pink[50],
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.pink[300],
+                        child: Text('${index + 1}'),
+                      ),
+                                 
+                    title: Text(parkingPass['parkingLocation'] ?? 'Unknown Location'),
+                    subtitle: Text('Spot No: ${parkingPass['parkingSpotNo']} - Date: ${parkingPass['date']}'),
+                    trailing: Text('\$${parkingPass['amountPaid']}'),
+                  ),
+                  ),
+                  Positioned(
+                        width: 30,
+                        bottom: 10,
+                        right: 3,
+                        child: InkWell(
+                          onTap: () => deleteParkingPass(parkingPass['id']),
+                          child: Icon(Icons.delete, color: Colors.red[800], size: 25),
+                        ),
+                      ),
+                    ],
+                  );
               },
             );
           } else {
